a8d4a8a0c4d87383494014a85040852ccfd5bae8
Chibuzor Emmanuel <66841449+chibuzor9@users.noreply.github.com>
2024-12-05 14:57:12 +0100

    fix: Update table names and foreign key references for consistency; add users table with initial data

--- schema.sql
+++ schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE student(
     parent_no VARCHAR(20) NOT NULL 
 ); 
 
-CREATE TABLE Incident( 
+CREATE TABLE incident( 
     id INT PRIMARY KEY AUTO_INCREMENT, 
     Incident_name VARCHAR(50) NOT NULL, 
     Incident_date DATE NOT NULL, 
@@ -18,24 +18,24 @@ CREATE TABLE Incident(
     Incident_sl VARCHAR(50) NOT NULL 
 ); 
 
-CREATE TABLE Disciplinary_Record ( 
+CREATE TABLE record ( 
     id INT PRIMARY KEY AUTO_INCREMENT, 
     Disciplinary_Record_Description TEXT NOT NULL, 
     Disciplinary_Record_status VARCHAR(50) NOT NULL, 
     StudentID INT, 
     IncidentID INT, 
-    CONSTRAINT fk_student FOREIGN KEY(StudentID) REFERENCES Students(StudentID) ON DELETE CASCADE ON UPDATE CASCADE, 
-    CONSTRAINT fk_incident FOREIGN KEY(IncidentID) REFERENCES Incident(IncidentID) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY(StudentID) REFERENCES student(id) ON DELETE CASCADE ON UPDATE CASCADE, 
+    FOREIGN KEY(IncidentID) REFERENCES incident(id) ON DELETE CASCADE ON UPDATE CASCADE
 ); 
 
-CREATE TABLE Disciplinary_Action ( 
+CREATE TABLE action ( 
     id INT PRIMARY KEY AUTO_INCREMENT, 
     Disciplinary_Incident_Type VARCHAR(50) NOT NULL, 
     Disciplinary_action_Taken TEXT NOT NULL, 
     Disciplinary_Terms VARCHAR(50) NOT NULL 
 ); 
 
-CREATE TABLE Admin ( 
+CREATE TABLE admin ( 
     id INT PRIMARY KEY AUTO_INCREMENT, 
     Admin_name VARCHAR(50) NOT NULL, 
     Admin_date DATE NOT NULL, 
@@ -43,9 +43,44 @@ CREATE TABLE Admin (
     Admin_sl VARCHAR(50) NOT NULL 
 ); 
 
-CREATE TABLE Appeal (
+CREATE TABLE appeal (
     id INT PRIMARY KEY AUTO_INCREMENT, 
     Appeal_name VARCHAR(50) NOT NULL, 
     Appeal_reason TEXT NOT NULL, 
-    Appeal_Status VARCHAR(50) NOT NULL 
+    Appeal_Status VARCHAR(50) NOT NULL,
+    StudentID INT,
+    IncidentID INT,
+    FOREIGN KEY(StudentID) REFERENCES student(id) ON DELETE CASCADE ON UPDATE CASCADE, 
+    FOREIGN KEY(IncidentID) REFERENCES incident(id) ON DELETE CASCADE ON UPDATE CASCADE 
 ); 
+
+
+
+USE users_db2;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('student', 'admin') NOT NULL,
+    PRIMARY KEY (id, username)
+);
+
+INSERT INTO users (id, username, password, role) 
+VALUES (1, 'admin', 'axios', 'admin');
+
+INSERT INTO users (id, username, password, role) 
+VALUES (2, '22/0060', 'axios', 'student');
+
+INSERT INTO users (id, username, password, role) 
+VALUES (2, '22/0035', 'axios', 'student');
+
+ALTER TABLE users
+ADD COLUMN StaffID INT NULL,
+ADD COLUMN StudentID INT NULL;
+
+ALTER TABLE users
+ADD FOREIGN KEY (StudentID) REFERENCES cosc333_db2.student(id);
+
+
+
